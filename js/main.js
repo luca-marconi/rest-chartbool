a78f2b3589d75e4a4235b6a25280e1ccdf92f434
Luca <luca-marconi@hotmail.com>
2020-04-07 19:47:48 +0200

    secondo grafico

--- js/main.js
+++ js/main.js
@@ -32,14 +32,11 @@ $(document).ready(function () {
             // console.log(vendita.amount);    // mostro il totale della singola vendita
             // console.log(vendita.date);      // mostro la data singola vendita
             var dataVendita = moment(vendita.date, 'DD-MM-YYYY');
-            console.log(dataVendita);
             var meseVendita = dataVendita.format('MMMM');
-            console.log(meseVendita);
-
             if (oggettoIntermedio[meseVendita] === undefined) {
                 oggettoIntermedio[meseVendita] = 0;
             }
-            console.log(oggettoIntermedio);
+            // console.log(oggettoIntermedio);
             oggettoIntermedio[meseVendita] += vendita.amount;
         }
 
@@ -58,18 +55,58 @@ $(document).ready(function () {
         data: {
             labels: mesi,
             datasets: [{
-                label: 'Fatturato Mensile ',
-                backgroundColor: 'rgba(0, 0, 0, 0.1)',
-                borderColor: 'lightblue',
+                label: 'Fatturato Mesi',
+                borderColor: 'lightgreen',
                 pointBackgroundColor: 'red',
-                pointBorderColor: 'red',
+                pointBorderColor: 'lightblue',
                 data: fatturato
             }]
         },
     });
 
+    $.ajax(settings).done(function (response) {
+        var vendite = response;
 
+        var oggettoIntermedio = {};
 
+        for (var i = 0; i < vendite.length; i++) {
+            var vendita = vendite[i];
+            // console.log(vendita);           // mostro l oggetto della singola vendita
+            // console.log(vendita.id);        // mostro l id della singola vendita
+            // console.log(vendita.salesman);  // mostro il venditore della singola vendita
+            // console.log(vendita.amount);    // mostro il totale della singola vendita
+            // console.log(vendita.date);      // mostro la data singola vendita
+            var nomeVenditore = vendita.salesman;
+            var fatturatoVenditore = vendita.amount
+            if (oggettoIntermedio[nomeVenditore] === undefined) {
+                oggettoIntermedio[nomeVenditore] = 0;
+            }
+            console.log(oggettoIntermedio);
+            oggettoIntermedio[nomeVenditore] += vendita.amount;
+        }
+
+        var nomiVenditori = [];
+        var fatturatoVenditori = [];
+
+        for (var key in oggettoIntermedio) {
+            nomiVenditori.push(key);
+            fatturatoVenditori.push(oggettoIntermedio[key]);
+        }
+        console.log(nomiVenditori);
+        console.log(fatturatoVenditori);
+
+        var ctx = $('#grafico2');
+        var chart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                datasets: [{
+                    data: fatturatoVenditori,
+                    backgroundColor: ['lightyellow', 'lightgreen', 'lightcoral', 'lightblue']
+                }],
+                labels: nomiVenditori
+            },
+        });
+    });
 });
 
 
